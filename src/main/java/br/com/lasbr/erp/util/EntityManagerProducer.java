718665960927fe9c179c0729639e269f9239e0f5
Luciano Alves <lasbr_alves@outlook.com>
2024-03-08 21:28:14 -0300

    feat: added EntityManagerProducer for interceptor implementation.

---
+++ src/main/java/br/com/lasbr/erp/util/EntityManagerProducer.java
@@ -0,0 +1,30 @@
+package br.com.lasbr.erp.util;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+@ApplicationScoped
+public class EntityManagerProducer {
+
+	private EntityManagerFactory factory;
+	
+	public EntityManagerProducer() {
+		this.factory = Persistence.createEntityManagerFactory("LasBrPU");
+	}
+	
+	@Produces
+	@RequestScoped
+	public EntityManager createEntityManager() {
+		return this.factory.createEntityManager();
+	}
+	
+	public void closeEntityManager(@Disposes EntityManager manager) {
+		manager.close();
+	}
+}
