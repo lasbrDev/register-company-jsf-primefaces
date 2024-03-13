1bbcb3c6b47886e276ecc8f785c13ecc2c630034
Luciano Alves <lasbr_alves@outlook.com>
2024-03-13 16:58:23 -0300

    refactor: removed ManagementCompaniesBean test methods.

--- src/main/java/br/com/lasbr/erp/controller/ManagementCompaniesBean.java
+++ src/main/java/br/com/lasbr/erp/controller/ManagementCompaniesBean.java
@@ -1,40 +1,15 @@
 package br.com.lasbr.erp.controller;
 
 import java.io.Serializable;
-import java.util.Arrays;
 
-import javax.faces.model.SelectItem;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
-import br.com.lasbr.erp.model.Company;
-import br.com.lasbr.erp.model.CompanyType;
-
 @Named
 @ViewScoped
-public class ManagementCompaniesBean  implements Serializable {
+public class ManagementCompaniesBean implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
-	private Company company = new Company();
 	
-	public void save() {
-		System.out.println("Corporate Name: " + company.getCorporateName()
-				+ " - Trade Name: " + company.getTradeName()
-				+ " - Type: " + company.getCompanyType());
-	}
-	
-	public String help() {
-		return "HelpManagementCompanies?faces-redirect=true";
-	}
-	
-	public Company getCompany() {
-		return company;
-	}
-	
-	public SelectItem[] getCompanyTypes() {
-		return Arrays.stream(CompanyType.values())
-				.map(type -> new SelectItem(type, type.getDescription()))
-				.toArray(SelectItem[]::new);
-	}
 }
