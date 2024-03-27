e7ff3c30847a7cea25d7bf3a8829298da0d4ef02
Luciano Alves <lasbr_alves@outlook.com>
2024-03-27 09:51:10 -0300

    feat: added User entity.

---
+++ src/main/java/br/com/lasbr/erp/model/User.java
@@ -0,0 +1,145 @@
+package br.com.lasbr.erp.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "user")
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@NotEmpty(message = "O nome não pode estar vazio!")
+	@Column(nullable = false)
+	private String firstName;
+
+	@NotEmpty(message = "O sobrenome não pode estar vazio!")
+	@Column(nullable = false)
+	private String lastName;
+
+	@NotEmpty(message = "O e-mail não pode estar vazio!")
+	@Email(message = "E-mail inválido!")
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	@NotNull(message = "A data de nascimento não pode estar vazia!")
+	@Past(message = "Escolha uma data válida!")
+	@Column(nullable = false)
+	private Date birthDate;
+
+	@NotEmpty(message = "A senha não pode estar vazia")
+	@Size(min = 8, message = "A senha deve ter pelo menos 8 caracteres")
+	@Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "A senha deve ter no mínimo 8 caracteres, incluindo números, letras maiúsculas, minúsculas e caracteres especiais.")
+	@Column(nullable = false)
+	private String password;
+
+	@Column(name = "recovery_code")
+	private String recoveryCode;
+
+	@Column(name = "code_expiration")
+	private Date recoveryCodeExpiration;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRecoveryCode() {
+		return recoveryCode;
+	}
+
+	public void setRecoveryCode(String recoveryCode) {
+		this.recoveryCode = recoveryCode;
+	}
+
+	public Date getRecoveryCodeExpiration() {
+		return recoveryCodeExpiration;
+	}
+
+	public void setRecoveryCodeExpiration(Date recoveryCodeExpiration) {
+		this.recoveryCodeExpiration = recoveryCodeExpiration;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + "]";
+	}
+}
